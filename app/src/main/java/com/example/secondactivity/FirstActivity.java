83ce626192200beb97a05175289558227c181699
ivissivi <ivis31121999@gmail.com>
2021-11-29 20:55:50 +0200

    Dialog window

--- app/src/main/java/com/example/secondactivity/FirstActivity.java
+++ app/src/main/java/com/example/secondactivity/FirstActivity.java
@@ -2,7 +2,9 @@ package com.example.secondactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity implements View.OnClickListener{
+
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,8 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
         btnGoTo2nd.setOnClickListener(this); // calling onClick() method
         Button btnDialog = (Button) findViewById(R.id.btnDialog);
         btnDialog.setOnClickListener(this); // calling onClick() method
+        builder = new AlertDialog.Builder (this);
+
     }
 
     @Override
@@ -35,16 +41,29 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
                 switchActivities();
                 break;
             case R.id.btnDialog:
-                toast.setText("Show dialog");
-                toast.show();
+                builder.setTitle("Alert!!!")
+                        .setMessage("Yes?")
+                        .setCancelable(true)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                toast.setText("You clicked OK");
+                                toast.show();
+                            }
+                        })
+                        .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .show();
                 break;
         }
-
-        }
+    }
 
     private void switchActivities() {
         Intent switchActivityIntent = new Intent(this, SecondActivity.class);
         startActivity(switchActivityIntent);
     }
-
 }
